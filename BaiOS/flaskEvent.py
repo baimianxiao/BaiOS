2237bd8f25c8d1ea08d4db7a215bfd7942433815
baimianxiao <67865300+baimianxiao@users.noreply.github.com>
2021-12-05 02:06:33 +0800

    update flaskEvent.py

--- BaiOS/flaskEvent.py
+++ BaiOS/flaskEvent.py
@@ -9,13 +9,22 @@ app = Flask(__name__)
 def cqhttp_post():
     data = request.get_data()
     json_data = json.loads(data.decode('utf-8'))
-    print(json_data)
-    if 'message_type' in json_data:
-        print(json_data['raw_message'])
-    return ""
+    shortcut_do=event_itemize(json_data)
+    return shortcut_do
 
 
-# def event_itemize(json_data):
-# if
+def event_itemize(json_data):
+  if '' in json_data:
+    
+  elif '' in json_data:
+    
+  elif '' in json_data:
+    
+  else:
+    return ''
+  
+  return shortcut_do
+
+
 if __name__ == "__main__":
     app.run(host='127.0.0.1', port=5701)
